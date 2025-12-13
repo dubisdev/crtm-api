3191ca891fde6cef20dd714fa091fbb6e92a8aa3
dubisdev <dubisdev@gmail.com>
2025-12-13 22:21:45 +0100

    chore: add some extra endpoints just for documentation

--- src/shared/constants/api.constants.ts
+++ src/shared/constants/api.constants.ts
@@ -3,6 +3,13 @@ export const CRTM_API = {
     ENDPOINTS: {
         STOP_INFORMATION: "GetStopsInformation.php",
         LINE_INFORMATION: "GetLines.php",
-        STOP_TIMES: "GetStopsTimes.php"
+        STOP_TIMES: "GetStopsTimes.php",
+
+        // Other endpoints that might be useful in the future
+        TRANSPORT_MODES: "GetModes.php",
+        MUNICIPALITIES: "GetMunicipalities.php",
+        OFFICES: "https://crtm.es/widgets/Offices.json",
+        INCIDENTS: "https://crtm.es/widgets/api/GetIncidentsAffectations.php?mode=6&codLine=6__M1___",
+        FULL_ITINERARY: "https://crtm.es/widgets/api/GetLinesInformation.php?activeItinerary=1&codLine=6__M1___"
     }
 } as const;
